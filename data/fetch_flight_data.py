1a5051decfb861e0d97806c8e049df4d7a34aff9
RiCEmare <rikace.panta@gmail.com>
2024-05-18 15:36:14 +0545

    Add API call requests

--- data/fetch_flight_data.py
+++ data/fetch_flight_data.py
@@ -1,6 +1,17 @@
+import random
 import requests
-from config.config import FLIGHT_API_URL
+from config.config import FLIGHT_LIB_URL, FLIGHT_DETAIL_URL, FLIGHT_LIB_PARAMS, FLIGHT_API_HEADERS
+
 
 def fetch_flight_data():
-    response = requests.get(FLIGHT_API_URL, timeout=10)  # Add timeout argument
-    return response.json()
+    response = requests.get(FLIGHT_LIB_URL, headers=FLIGHT_API_HEADERS,
+                            params=FLIGHT_LIB_PARAMS,timeout=20)
+    data = response.json()
+    selected_flight = random.choice(data['aircraft'])
+    flight_id = selected_flight[0]
+
+    FLIGHT_DETAIL_PARAMS = {"flight": flight_id}
+    detail_response = requests.get(FLIGHT_DETAIL_URL, headers=FLIGHT_API_HEADERS,
+        params=FLIGHT_DETAIL_PARAMS,timeout=20)
+    detail_data = detail_response.json()
+    return detail_data
--- tweets/tweet_flights.py
+++ tweets/tweet_flights.py
@@ -1,19 +1,21 @@
 import tweepy
 from config.config import API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
-#from data.fetch_flight_data import fetch_flight_data
+from data.fetch_flight_data import fetch_flight_data
 
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+client = tweepy.Client(consumer_key=API_KEY, consumer_secret=API_SECRET,
+    access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
 
-def format_tweet():
-    flight_no = 'test-flight-123'
-    status = 'on time'
-    time = '12:00 PM'
-    return f"Flight {flight_no} is {status} at {time}."
+def format_tweet(detail_data):
+    flight_number = detail_data["identification"]["number"]["default"]
+    departure_airport = detail_data["airport"]["origin"]["name"]
+    arrival_airport = detail_data["airport"]["destination"]["name"]
+    scheduled_departure_time = detail_data["time"]["scheduled"]["departure"]
+    scheduled_arrival_time = detail_data["time"]["scheduled"]["arrival"]
+
+    return f"Flight {flight_number} from {departure_airport} to {arrival_airport} is scheduled to depart at {scheduled_departure_time} and arrive at {scheduled_arrival_time}."
 
 def tweet_flights():
-    #flights = fetch_flight_data()
-    #for flight in flights:
-    tweet = format_tweet()
-    api.update_status(tweet)
+    detail_data = fetch_flight_data()
+    tweet = client.create_tweet(
+    text=format_tweet(detail_data)
+)
