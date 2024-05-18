91c5a00a03031ff176ddbd553380f59cac0d76f6
RiCEmare <rikace.panta@gmail.com>
2024-05-18 22:12:21 +0545

    Refactor

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,9 +1,11 @@
-import schedule
+''' This file is used to schedule the tweets '''
 import time
-from tweets.tweet_flights import tweet_flights
+import schedule
+from tweets.tweet_flights import tweet
 
 def schedule_tweets():
-    schedule.every(30).minutes.do(tweet_flights)
+    ''' This function schedules the tweets to be posted every 30 minutes. '''
+    schedule.every(30).minutes.do(tweet)
     while True:
         schedule.run_pending()
         time.sleep(1)
