5e24896421d438c90909d73cfbb5aed6a4b3cb65
RiCEmare <rikace.panta@gmail.com>
2024-05-18 23:44:22 +0545

    Update tweet formatter to include flight status

--- tweets/tweet_formatter.py
+++ tweets/tweet_formatter.py
@@ -8,11 +8,23 @@ def timestamp_to_datetime(timestamp):
 
 def format_tweet(detail_data):
     ''' This function creates the tweet from the detail data.'''
+
     airline = detail_data["airline"]["short"]
     flight_number = detail_data["identification"]["number"]["default"]
     departure_airport = detail_data["airport"]["origin"]["name"]
     arrival_airport = detail_data["airport"]["destination"]["name"]
     scheduled_departure_time = timestamp_to_datetime(detail_data["time"]["scheduled"]["departure"])
     scheduled_arrival_time = timestamp_to_datetime(detail_data["time"]["scheduled"]["arrival"])
-
-    return f"{airline} Flight {flight_number} from {departure_airport} to {arrival_airport} is scheduled to depart at {scheduled_departure_time} and arrive at {scheduled_arrival_time}."
+    status = detail_data["status"]["generic"]["status"]["text"]
+    if status == "arrived":
+        return f"{airline} Flight {flight_number} from {departure_airport} to {arrival_airport} arrived at {scheduled_arrival_time}."
+    elif status == "scheduled":
+        return f"{airline} Flight {flight_number} from {departure_airport} to {arrival_airport} is scheduled to depart at {scheduled_departure_time} and arrive at {scheduled_arrival_time}."
+    elif status == "cancelled":
+        return f"{airline} Flight {flight_number} from {departure_airport} to {arrival_airport} has been cancelled."
+    elif status == "diverted":
+        return f"{airline} Flight {flight_number} from {departure_airport} to {arrival_airport} has been diverted."
+    elif status == "delayed":
+        return f"{airline} Flight {flight_number} from {departure_airport} to {arrival_airport} has been delayed."
+    else:
+        return f"{airline} Flight {flight_number} from {departure_airport} to {arrival_airport} is {status}."
--- utils/scheduler.py
+++ utils/scheduler.py
@@ -5,7 +5,7 @@ from tweets.tweet_flights import tweet
 
 def schedule_tweets():
     ''' This function schedules the tweets to be posted every 30 minutes. '''
-    schedule.every(30).minutes.do(tweet)
+    schedule.every(180).minutes.do(tweet)
     while True:
         schedule.run_pending()
         time.sleep(1)
