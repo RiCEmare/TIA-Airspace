c1662ca1724c47ea13cf42b333e33bda47a7075f
RiCEmare <rikace.panta@gmail.com>
2024-05-18 15:35:18 +0545

    Update flight API URL and headers

--- config/config.example.py
+++ config/config.example.py
@@ -3,15 +3,16 @@ API_SECRET = ''
 ACCESS_TOKEN = ''
 ACCESS_TOKEN_SECRET = ''
 CALLBACK_URL = ''
-FLIGHT_API_URL = 'https://flight-radar1.p.rapidapi.com/flights/list-in-boundary'
+FLIGHT_LIB_URL = 'https://flight-radar1.p.rapidapi.com/flights/list-in-boundary'
+FLIGHT_DETAIL_URL = 'https://flight-radar1.p.rapidapi.com/flights/detail'
 FLIGHT_API_HEADERS = {
     "X-RapidAPI-Key": "",
     "X-RapidAPI-Host": "flight-radar1.p.rapidapi.com"
 }
-FLIGHT_QUERY_PARAMS = {
-    "bl_lat": "27.604662",
-    "bl_lng": "85.135186",
-    "tr_lat": "27.810135",
-    "tr_lng": "85.494301",
+FLIGHT_LIB_PARAMS = {
+    "bl_lat": "27.569257",
+    "bl_lng": "85.154247",
+    "tr_lat": "27.839468",
+    "tr_lng": "85.555804",
     "limit": "300"
 }
