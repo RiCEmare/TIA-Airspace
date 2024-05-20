d62bee90e125ce826d19a600e60bd4ecb9ae62e2
RiCEmare <rikace.panta@gmail.com>
2024-05-20 17:23:26 +0545

    Fix: failsafe for flight image

--- data/fetch_flight_data.py
+++ data/fetch_flight_data.py
@@ -9,9 +9,13 @@ def fetch_flight_data():
     response = requests.get(FLIGHT_LIB_URL, headers=FLIGHT_API_HEADERS,
                             params=FLIGHT_LIB_PARAMS,timeout=20)
     data = response.json()
-    selected_flight = random.choice(data['aircraft'])
-    flight_id = selected_flight[0]
 
+    if data['aircraft'] != []:
+        selected_flight = random.choice(data['aircraft'])
+        flight_id = selected_flight[0]
+    else:
+        return -1
+    
     flight_detail_params = {"flight": flight_id}
     detail_response = requests.get(FLIGHT_DETAIL_URL, headers=FLIGHT_API_HEADERS,
         params=flight_detail_params,timeout=20)
