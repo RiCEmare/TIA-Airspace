56c4195c2fcadbf64b1e8ac3c1ef69e5e5bf27ab
RiCEmare <rikace.panta@gmail.com>
2025-03-02 23:08:49 +0545

    fix: gemini support

--- tweets/tweet_flights.py
+++ tweets/tweet_flights.py
@@ -8,19 +8,21 @@ from data.fetch_flight_data import fetch_flight_data
 client_v1 = get_twitter_conn_v1()
 client_v2 = get_twitter_conn_v2()
 
-#add code for remove previous image and download new image and set to media_path
 def tweet():
     ''' This function tweets the flight details of the user. '''
     if fetch_flight_data() != -1:
         detail_data = fetch_flight_data()
         tweet_text = format_tweet(detail_data)
     else:
-        tweet_text = "Currently there are no flights in the airspace. Here's a picture of a cat instead. 😻"
-        url = 'https://cataas.com/cat'
-        media_path = download_image(url,aircraft=False)
-        media = client_v1.media_upload(filename=media_path)
-        media_id = media.media_id
-        return client_v2.create_tweet(text=tweet_text, media_ids=[media_id])
+        if random.random() < 0.5:  # 50% chance of creating cat post
+            tweet_text = "Currently there are no flights in the airspace. Here's a picture of a cat instead. 😻"
+            url = 'https://cataas.com/cat'
+            media_path = download_image(url,aircraft=False)
+            media = client_v1.media_upload(filename=media_path)
+            media_id = media.media_id
+            return client_v2.create_tweet(text=tweet_text, media_ids=[media_id])
+        else:
+            return
 
     thumbnail = safe_get(detail_data, ['aircraft', 'images', 'thumbnails'])
     if thumbnail != 'null':
--- tweets/tweet_formatter.py
+++ tweets/tweet_formatter.py
@@ -1,10 +1,13 @@
-''' This module contains functions to format the tweet data. '''
+""" This module contains functions to format the tweet data. """
+
 from datetime import datetime, timezone, timedelta
-import google.generativeai as genai
+from google import genai
+from google.genai import types
 from config.config import GEMINI_API_KEY
 
-def safe_get(data, keys, default='null'):
-    ''' This function safely gets the value from the json.'''
+
+def safe_get(data, keys, default="null"):
+    """This function safely gets the value from the json."""
     for key in keys:
         if isinstance(data, dict):
             data = data.get(key, default)
@@ -12,36 +15,43 @@ def safe_get(data, keys, default='null'):
             return default
     return data if data is not None else default
 
+
 def format_timestamp(timestamp):
-    ''' This function converts the timestamp to a UTC+5:45 datetime format.'''
-    if timestamp == 'null':
-        return 'null'
+    """This function converts the timestamp to a UTC+5:45 datetime format."""
+    if timestamp == "null":
+        return "null"
     utc_time = datetime.fromtimestamp(timestamp, tz=timezone.utc)
     nepal_time = utc_time.astimezone(timezone(timedelta(hours=5, minutes=45)))
     return nepal_time.strftime("%I:%M %p")
 
-def format_tweet(detail_data):
-    ''' This function creates the tweet from the detail data.'''
 
-    flight_number = safe_get(detail_data, ['identification', 'number', 'default'])
-    airline_name = safe_get(detail_data, ['airline', 'name'])
-    origin_name = safe_get(detail_data, ['airport', 'origin', 'name'])
-    destination_name = safe_get(detail_data, ['airport', 'destination', 'name'])
-    scheduled_departure = safe_get(detail_data, ['time', 'scheduled', 'departure'])
-    scheduled_arrival = safe_get(detail_data, ['time', 'scheduled', 'arrival'])
-    status_text = safe_get(detail_data, ['status', 'text'])
-    real_departure = safe_get(detail_data, ['time', 'real', 'departure'])
-    real_arrival = safe_get(detail_data, ['time', 'real', 'arrival'])
-    estimated_arrival = safe_get(detail_data, ['time', 'estimated', 'arrival'])
+def format_tweet(detail_data):
+    """This function creates the tweet from the detail data."""
+
+    flight_number = safe_get(detail_data, ["identification", "number", "default"])
+    airline_name = safe_get(detail_data, ["airline", "name"])
+    origin_name = safe_get(detail_data, ["airport", "origin", "name"])
+    destination_name = safe_get(detail_data, ["airport", "destination", "name"])
+    scheduled_departure = safe_get(detail_data, ["time", "scheduled", "departure"])
+    scheduled_arrival = safe_get(detail_data, ["time", "scheduled", "arrival"])
+    status_text = safe_get(detail_data, ["status", "text"])
+    real_departure = safe_get(detail_data, ["time", "real", "departure"])
+    real_arrival = safe_get(detail_data, ["time", "real", "arrival"])
+    estimated_arrival = safe_get(detail_data, ["time", "estimated", "arrival"])
     scheduled_departure_time = format_timestamp(scheduled_departure)
     scheduled_arrival_time = format_timestamp(scheduled_arrival)
     real_departure_time = format_timestamp(real_departure)
     real_arrival_time = format_timestamp(real_arrival)
     estimated_arrival_time = format_timestamp(estimated_arrival)
 
-    genai.configure(api_key=GEMINI_API_KEY)
-    model = genai.GenerativeModel('gemini-pro')
-    while True:
+    sys_instruct = """
+    You are twitter bot that provides information about different flights.
+    You have to create a tweet for a bot which posts info of a different flight from the given information.
+    The tweet should be informative, fun, and under 275 characters. Create sentences. No hashtags or links.
+    Include flag emojis of the origin and destination countries. No need for further updates.
+    """
+    client = genai.Client(api_key=GEMINI_API_KEY)
+    for _ in range(2):
         prompt = f"""
         Create a tweet for a bot which posts info of a different flight each time.
         following are flight details of a randomly selected flight.
@@ -55,9 +65,13 @@ def format_tweet(detail_data):
         Real Departure Time: {real_departure_time}
         Real Arrival Time: {real_arrival_time}
         Estimated Arrival Time: {estimated_arrival_time}
-        The tweet should be informative, fun, and under 275 characters. Create sentences. No hashtags or links. Include flag emojis of the origin and destination countries. No further updates. 
         """
-        response = model.generate_content(prompt)
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            config=types.GenerateContentConfig(system_instruction=sys_instruct),
+            contents=[prompt],
+        )
         if len(response.text)<=280:
             break
+    print(response.text)
     return response.text
